d1c200be78dbd3826f70d182837911cbb032e213
yankyalvarezm <alvarezyanky7#gmail.com>
2024-04-05 17:37:15 -0400

    change host

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,5 +1,6 @@
-const API_URL = "http://localhost:3000";
+// const API_URL = "http://localhost:3000";
 
 // const TEST_HOST = "http://localhost:5173";
+const API_URL = "https://spotmeup-s.adaptable.app"
 
 export {API_URL}
